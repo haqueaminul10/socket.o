de0b550c75f135004876211e5a6d6133e898a81a
haqueaminul10 <115152893+haqueaminul10@users.noreply.github.com>
2025-10-21 17:26:11 +0600

    socket connection

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3001',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`📌 ~ socket: server`, socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected:server', socket.id);
+  });
+});
+
+// app.get('/', (req, res) => {
+//   res.send('Socket.IO server running');
+// });
+
+server.listen(4000, () => {
+  console.log(`Server running on http://localhost:4000`);
+});
